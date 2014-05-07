236e167872173c36a5b2adb4df8d9b290234c647
Bell_staymen02 <tester123113180@gmail.com>
2014-05-07 14:23:27 +0900

    update_name

---
+++ update_name.rb
@@ -0,0 +1,54 @@
+# Coding: UTF-8
+require 'rubygems'
+require 'twitter'
+require 'tweetstream'
+
+# --- Define ---
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
+
+
+# -- Twitter ---
+
+TweetStream.configure do |config|
+    config.consumer_key       = CONSUMER_KEY
+    config.consumer_secret    = CONSUMER_SECRET
+    config.oauth_token        = ACCESS_TOKEN
+    config.oauth_token_secret = ACCESS_SECRET
+end
+
+   restclient = Twitter::REST::Client.new do |config|
+    config.consumer_key        = CONSUMER_KEY 
+    config.consumer_secret     = CONSUMER_SECRET
+    config.access_token        = ACCESS_TOKEN
+    config.access_token_secret = ACCESS_SECRET
+end
+
+# --- main ---
+client = TweetStream::Client.new.track('@Bell_staymen02') do |status|
+    #client = TweetStream::Client.new.userstream do |status|
+    text = status.text
+    if text.start_with? "RT"
+        next
+    elsif text =~ /^.*[[:blank:]]*[@＠]Bell_staymen02[[:blank:]]*update_name[[:blank:]]*/
+        newname = text.gsub(/^.*[[:blank:]]*[@＠]Bell_staymen02[[:blank:]]*update_name[[:blank:]]*/,"")
+    elsif text =~ /[(（【][@＠]Bell_staymen02[[:blank:]]*[）)】][[:blank:]]*$/
+        newname = text.gsub(/[(（【][@＠]Bell_staymen02[[:blank:]]*[）)】][[:blank:]]*$/,"")
+        newname = newname.delete("~")
+    end
+
+    if newname.length <= 20
+        tweet = "@#{status.user.screen_name} #{newname} に改名しました。"
+        restclient.favorite(status.id)
+        restclient.update_profile({:name => newname})
+        restclient.update(tweet, :in_reply_to_status_id => status.id)
+        puts "@#{status.user.screen_name}によって #{newname} に改名させられた。"
+    else
+         tweet_err = "@#{status.user.screen_name} 長すぎるんじゃボケ"
+         restclient.favorite(status.id)
+         restclient.update(tweet_err, :in_reply_to_status_id => status.id)
+         puts "@#{status.user.screen_name}:ERROR(Newname too long)"
+    end
+end
